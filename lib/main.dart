be5716d1c81d68306ea584526446bef2cb253e82
aditi-g21 <goyal.aditi121@gmail.com>
2020-03-03 20:20:13 +0530

    Ball Functionalities Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,38 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+          home: Scaffold(
+        backgroundColor: Colors.blueGrey[300],
+        appBar: AppBar(
+          title: Text('Magic 8 Ball'),
+          backgroundColor: Colors.blueGrey[800],
+        ),
+        body: MyBall(),
+      )),
+    );
+
+class MyBall extends StatefulWidget {
+  @override
+  _MyBallState createState() => _MyBallState();
+}
+
+class _MyBallState extends State<MyBall> {
+  int ball = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: FlatButton(
+          onPressed: () {
+            setState(() {
+              ball = Random().nextInt(5) + 1;
+            });
+          },
+          child: Image.asset('images/ball$ball.png'),
+        ),
       ),
     );
+  }
+}
